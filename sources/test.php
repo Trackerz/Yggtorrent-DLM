eb23ac068e89fa4a768a2dd8b0629637ad0d5b50
Trackerz <11777341+Trackerz@users.noreply.github.com>
2019-12-04 23:33:30 +0100

    Update test.php

--- sources/test.php
+++ sources/test.php
@@ -9,26 +9,28 @@ $ygg = new YGGTorrentDLM();
 $curl = curl_init();
 
 if($ygg->VerifyAccount($username, $password)) {
-    echo '<p style="color:green">CONNECTED</p>';
+
+    echo '<p style="color:green">CONNECTED</p>';    
+
+    $ygg->prepare($curl, $request, $username, $password);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    $plugin = new Plugin();
+    $count = $ygg->parse($plugin, $response);
+
+    echo '</br>Total results : ' . $plugin->Total() . '</br></br>';
+    echo '<pre>';
+    var_dump($plugin->results);
+    echo '</pre>';
+    
 } else {
     echo '<p style="color:red">NOT CONNECTED</p>';
 }
 
-$ygg->prepare($curl, $request, $username, $password);
-
-$response = curl_exec($curl);
-curl_close($curl);
-
-$plugin = new Plugin();
-$count = $ygg->parse($plugin, $response);
-
-echo '</br>Total results : ' . $plugin->Total() . '</br></br>';
-echo '<pre>';
-var_dump($plugin->results);
-echo '</pre>';
-
 class Plugin {
-    
+        
     public $results;
 
     public function AddResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category) {
