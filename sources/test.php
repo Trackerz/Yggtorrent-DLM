941941330a3f327867201ba7f0ab01d69200cff4
Trackerz <11777341+Trackerz@users.noreply.github.com>
2020-07-27 12:01:56 +0200

    Update test.php

--- sources/test.php
+++ sources/test.php
@@ -1,6 +1,10 @@
 <?php
 require('search.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $username = '';
 $password = '';
 $request = 'game of throne';
