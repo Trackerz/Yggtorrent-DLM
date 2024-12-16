791e40a7106321a74374a81403f476e6e74d4564
Trackerz <11777341+Trackerz@users.noreply.github.com>
2024-12-16 18:18:39 +0100

    fix headers

--- ygg.php
+++ ygg.php
@@ -5,23 +5,23 @@
  * /!\ Ne pas modifier ce fichier /!\
  */
 
-$curl = curl_init();
+$curl = curl_init();     
 
 curl_setopt_array($curl, [
     CURLOPT_HTTPHEADER => [
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,/;q=0.8',
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
         'TE: trailers',
         'Upgrade-Insecure-Requests: 1',
+        'Cookie: account_created=true; cf_clearance=; ygg_='
     ],
-    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
     CURLOPT_URL => $_GET['torrent'],
     CURLOPT_SSL_VERIFYPEER => false,
-    CURLOPT_SSL_VERIFYHOST => 2,
+    CURLOPT_SSL_VERIFYHOST => false,
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_FOLLOWLOCATION => true,      
     CURLOPT_COOKIEFILE => $_GET['cookie'],
-    CURLOPT_COOKIEJAR => $GET['cookie'],
-    CURLOPT_COOKIE => 'account_created=true; cf_clearance=; ygg=',
+    CURLOPT_COOKIEJAR => $_GET['cookie']
 ]);
 
 $content = curl_exec($curl);
